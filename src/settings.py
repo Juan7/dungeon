84a2e2febc58d4da13488b118ad2e09fa02fb2be
Juan7 <juan.gomez.q@gmail.com>
2016-05-17 20:11:16 -0500

    parameters on dict over settings

--- src/settings.py
+++ src/settings.py
@@ -15,3 +15,24 @@ MAX_WIDTH = 25
 MAX_HEIGHT = 25
 MAX_ENEMIES = 50
 LIVES = 5
+
+PARAMETERS = [
+    {'width': {
+            'message': '- Set width: (Max value = ' + str(MAX_WIDTH) + ' )',
+            'ex_message': '- Set a correct width: ',
+            'max': MAX_WIDTH,
+        }
+    },
+    {'height': {
+            'message': '- Set height: (Max value = ' + str(MAX_WIDTH) + ' )',
+            'ex_message': '- Set a correct height: ',
+            'max': MAX_HEIGHT,
+        }
+    },
+    {'enemies_number': {
+            'message': '- Set enemies number: (Max value = ' + str(MAX_ENEMIES) + ' )',
+            'ex_message': '- Set a correct enemies number: ',
+            'max': MAX_ENEMIES,
+        }
+    },
+]
